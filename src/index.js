2b3a374ffcf0f66fcd7aea33f45cbb52026ca47a
Kélison Rosendo da Rocha <kelisonrosendo@gmail.com>
2020-07-29 20:46:09 -0300

    commit aula 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import Pagina404 from './pages/404';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} exact/>
-      <Route component={() => (<div>Página não encontrada.</div>)} />
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/pages/404/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pagina404() {
+    return (
+        <div>
+            Página não encontrada.
+        </div>
+    );
+}
+
+export default Pagina404;
